c7b256ed5d2f288e59a1ad4c03628e95e383ae5c
2025sudheerprajapati <2025.sudheer.prajapati@ves.ac.in>
2026-02-09 10:31:26 +0530

    Add files via upload

---
+++ InsertionSortPassTime.java
@@ -0,0 +1,22 @@
+public class InsertionSortPassTime {
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 8, 4, 2 };
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        System.out.println("Sorted array:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
