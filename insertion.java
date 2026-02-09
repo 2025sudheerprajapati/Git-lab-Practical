75017d32f903183247f7defdef7221e269587038
2025sudheerprajapati <2025.sudheer.prajapati@ves.ac.in>
2026-02-09 10:29:35 +0530

    Update insertion.java

--- insertion.java
+++ insertion.java
@@ -1,44 +1,25 @@
-import java.util.Scanner;
+public class InsertionSortPassTime {
+public static void main(String[] args) {
+int[] arr = {5, 3, 8, 4, 2};
+int n = arr.length;
 
-class InsertionSortPassTime {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+for (int i = 1; i < n; i++) {
+int key = arr[i];
+int j = i - 1;
 
-        System.out.print("Enter number of elements: ");
-        int n = sc.nextInt();
 
-        int[] arr = new int[n];
-
-        System.out.println("Enter elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        long startTime = System.nanoTime(); // Start time
-
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-
-            // Print pass
-            System.out.print("Pass " + i + ": ");
-            for (int k = 0; k < n; k++) {
-                System.out.print(arr[k] + " ");
-            }
-            System.out.println();
-        }
-
-        long endTime = System.nanoTime(); // End time
-
-        System.out.println("Execution Time (Insertion Sort): "
-                + (endTime - startTime) + " nanoseconds");
-    }
+while (j >= 0 && arr[j] > key) {
+arr[j + 1] = arr[j];
+j--;
+}
+arr[j + 1] = key;
 }
 
+
+System.out.println("Sorted array:");
+for (int x : arr) {
+System.out.print(x + " ");
+}
+}
+}
